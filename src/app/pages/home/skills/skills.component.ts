b88dba538bca7ab6cfaac8d254e7e9b04a5f2365
isaac <isaacbejaranopro@gmail.com>
2024-03-01 21:25:31 +0100

    refactor skill component

--- src/app/pages/home/skills/skills.component.ts
+++ src/app/pages/home/skills/skills.component.ts
@@ -12,36 +12,34 @@ export class SkillsComponent implements OnInit {
   divider = 'divider-dark'; // -> <app-divider> Input()
 
   // TOP SKILLS
-  skillsLang = store.getSkillsLang as object;
-  skillsJS = store.getSkillsJS as object;
+  skillsLang = store.getSkillsLang;
+  skillsJS = store.getSkillsJS;
   // skillsPHP = store.getSkillsPHP as object;
-  skillsCSS = store.getSkillsCSS as object;
-  skillsDB = store.getSkillsDB as object;
-  XaaS = store.getSkillsXaaS as object;
-  techniques = store.getSkillsTechniques as object;
-  packages = store.getSkillsPackages as object;
-  wordpress = store.getSkillsWordpress as object;
-  git = store.getSkillsGit as object;
-  TDD = store.getSkillsTDD as object;
-  state = store.getSkillsState as object;
-  webpack = store.getSkillsWebpack as object;
+  skillsCSS = store.getSkillsCSS;
+  skillsDB = store.getSkillsDB;
+  XaaS = store.getSkillsXaaS;
+  techniques = store.getSkillsTechniques;
+  packages = store.getSkillsPackages;
+  wordpress = store.getSkillsWordpress;
+  git = store.getSkillsGit;
+  TDD = store.getSkillsTDD;
+  state = store.getSkillsState;
+  webpack = store.getSkillsWebpack;
 
   // SKILLS
-  skillsLang_basic = store.getSkillsLang_basic as object;
-  skillsJS_basic = store.getSkillsJS_basic as object;
-  skillsPHP_basic = store.getSkillsPHP_basic as object;
-  skillsCSS_basic = store.getSkillsCSS_basic as object;
-  skillsDB_basic = store.getSkillsDB_basic as object;
-  XaaS_basic = store.getSkillsXaaS_basic as object;
-  techniques_basic = store.getSkillsTechniques_basic as object;
-  packages_basic = store.getSkillsPackages_basic as object;
-  wordpress_basic = store.getSkillsWordpress_basic as object;
-  git_basic = store.getSkillsGit_basic as object;
-  TDD_basic = store.getSkillsTDD_basic as object;
-  state_basic = store.getSkillsState_basic as object;
-  webpack_basic = store.getSkillsWebpack_basic as object;
-
-  constructor() {}
+  skillsLang_basic = store.getSkillsLang_basic;
+  skillsJS_basic = store.getSkillsJS_basic;
+  skillsPHP_basic = store.getSkillsPHP_basic;
+  skillsCSS_basic = store.getSkillsCSS_basic;
+  skillsDB_basic = store.getSkillsDB_basic;
+  XaaS_basic = store.getSkillsXaaS_basic;
+  techniques_basic = store.getSkillsTechniques_basic;
+  packages_basic = store.getSkillsPackages_basic;
+  wordpress_basic = store.getSkillsWordpress_basic;
+  git_basic = store.getSkillsGit_basic;
+  TDD_basic = store.getSkillsTDD_basic;
+  state_basic = store.getSkillsState_basic;
+  webpack_basic = store.getSkillsWebpack_basic;
 
   ngOnInit(): void {
     $(function () {

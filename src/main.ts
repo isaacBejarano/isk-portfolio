e4ef3915a31728cdc7a374bf987bcaf8071f449a
Isaac Bejarano Fernandez <ibejaranof@uoc.edu>
2026-01-17 06:03:58 +0100

    Refactor modern features

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,4 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from '@app/app';
 import { appConfig } from '@app/app-config';
 
-// if (environment.production) {
-//   enableProdMode();
-// }
-
-// platformBrowserDynamic()
-//   .bootstrapModule(AppModule, {
-//     applicationProviders: [provideZoneChangeDetection()],
-//   })
-//   .catch((err) => console.error(err));
-
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));

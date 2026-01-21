fb8b827fa11f55b435aa83c2f24b37d3fb947b92
Isaac Bejarano Fernandez <ibejaranof@uoc.edu>
2026-01-21 01:36:02 +0100

    Mod header layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Follow } from '@app/follow/follow';
 import { Footer } from '@app/footer/footer';
-import { Navbar } from '@app/navbar/navbar';
+import { Navbar } from '@app/navbar/header';
 import { Scroll } from '@app/scroll/scroll';
 
 @Component({
---
+++ src/app/navbar/header.ts
@@ -0,0 +1,40 @@
+import {
+	ChangeDetectionStrategy,
+	Component,
+	DOCUMENT,
+	inject,
+	type OnInit,
+	signal,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { store } from '@app/app-store';
+import { hashIt } from '@app/utils/string-utils';
+
+@Component({
+	selector: 'isk-header',
+	templateUrl: './header.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [RouterLink],
+})
+export class Navbar implements OnInit {
+	navbar: Record<string, string> = store.getNavbar;
+	// faBars = faBars; FIXME:
+	hashIt = hashIt;
+
+	// DI
+	private readonly _doc = inject(DOCUMENT).defaultView;
+	protected readonly collapsed = signal(true);
+
+	// CTRL
+	////
+
+	ngOnInit(): void {
+		this.toggle(true);
+	}
+
+	protected toggle(init: boolean) {
+		const md = parseInt(this._doc.visualViewport.width.toFixed(0), 10);
+		if (md < 768) this.collapsed.update((v) => (init ? true : !v));
+		else this.collapsed.set(false);
+	}
+}
